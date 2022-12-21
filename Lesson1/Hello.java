48540eb474e41ccbeea04fd15875a8b8e1cc788c
DamirLapeiko <lapeiko02@mail.ru>
2022-12-21 19:07:57 +0300

    Hi

--- Lesson1/Hello.java
+++ Lesson1/Hello.java
@@ -1,5 +1,7 @@
+package Lesson1;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Piatro :D");
+        System.out.println("Lesson1.Hello Piatro :D");
     }
 }
