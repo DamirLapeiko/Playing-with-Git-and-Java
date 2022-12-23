5ecf9d9a695bb16c06dd0222935c7c690c219575
DamirLapeiko <lapeiko02@mail.ru>
2022-12-23 18:37:11 +0300

    HW done

--- Lesson2/HW_Lesson2.java
+++ Lesson2/HW_Lesson2.java
@@ -7,18 +7,14 @@ public class HW_Lesson2 {
         System.out.println("Здравствуйте, Вас приветствует Джарвис, введите свое имя для авторизации");
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextLine());
-        System.out.println("Hello, my friend!");
-
-
-//        System.out.println("Приветствую, Дамир, введите свой год рождения для подтверждения авторизации");
-//        Scanner sc = new Scanner(System.in);
-//        int age = sc.nextInt();
-//        if (age != 2002){
-//            System.out.println("Неправильный год рождения");
-//        }
-//        if (age == 2002) {
-//            System.out.println(sc.nextLine());
-//            System.out.println("Вы авторизовались, Вам сейчас " + (2022 - age) + ' ' + "лет");
-//        }
+        System.out.println("Приветствую, Дамир, введите свой год рождения для подтверждения авторизации");
+        int age = sc.nextInt();
+        if (age != 2002){
+            System.out.println("Неправильный год рождения");
+        }
+        if (age == 2002) {
+            System.out.println(sc.nextLine());
+            System.out.println("Вы авторизовались, Вам сейчас " + (2022 - age) + ' ' + "лет");
+        }
     }
 }
